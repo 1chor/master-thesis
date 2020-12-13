04c567899fad48a777caca872a829a5d2a29d77a
1chor <e1325347@student.tuwien.ac.at>
2020-12-13 15:08:15 +0100

    added classes from SoC project app
    removed connect button

--- empaticaE4/app/src/main/java/com/example/anwender/empaticae4/Configuration/ConfigActivity.java
+++ empaticaE4/app/src/main/java/com/example/anwender/empaticae4/Configuration/ConfigActivity.java
@@ -1,11 +1,14 @@
 package com.example.anwender.empaticae4.Configuration;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -23,22 +26,25 @@ import com.example.anwender.empaticae4.EWS.GraphSBP;
 import com.example.anwender.empaticae4.EWS.GraphSPO2;
 import com.example.anwender.empaticae4.EWS.GraphTemp;
 import com.example.anwender.empaticae4.EWS.Historical;
+import com.example.anwender.empaticae4.Main.MainActivity;
 import com.example.anwender.empaticae4.Main.Utility;
 import com.example.anwender.empaticae4.R;
 
-public class ConfigActivity extends AppCompatActivity {
+import org.json.JSONObject;
+
+import java.util.HashMap;
+
+public class ConfigActivity extends AppCompatActivity implements  NetworkManager.Callback, MsgProcessor.Callback, FabricManager.Callback {
 
     //classes
     private FragmentManager mFragManager;
     private NetworkManager mNetworkFragment;
     private MsgProcessor mMsgProcessor;
     private FabricManager mFabricManager;
-    private Util mUtil;
 
     //layout
     private TextView mConsole;
     private TextView mServerIP;
-    private Button mButtonConnect;
     private Button mButtonConfig;
     private RadioGroup radioGroupConfig;
     private RadioButton radioButtonSDFT;
@@ -51,12 +57,16 @@ public class ConfigActivity extends AppCompatActivity {
     private String blake2bHash;
     private String reconfigDriver;
 
+    private SharedPreferences mSharedPref;
     private boolean download;
+    private String repo_name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configuration);
+
+        mSharedPref = this.getPreferences(Context.MODE_PRIVATE);
 
         //initialise Textviews
         mConsole = (TextView) findViewById(R.id.console);
@@ -64,7 +74,6 @@ public class ConfigActivity extends AppCompatActivity {
         mServerIP = (TextView) findViewById(R.id.edit_serverIP);
 
         //initialise Buttons
-        mButtonConnect = (Button) findViewById(R.id.button_connect);
         mButtonConfig = (Button) findViewById(R.id.button_config);
 
         //initialise RadioButtons
@@ -87,7 +96,6 @@ public class ConfigActivity extends AppCompatActivity {
         mNetworkFragment = new NetworkManager();
         mMsgProcessor = new MsgProcessor(this);
         mFabricManager = new FabricManager(this);
-        mUtil = new Util(this);
         mFragManager = getSupportFragmentManager();
         mFragManager.beginTransaction().add(mNetworkFragment, "NetworkManager").commit();
 
@@ -98,21 +106,37 @@ public class ConfigActivity extends AppCompatActivity {
 
         download = false;
 
-        //SPO2 GRAPH BUTTON
-        oxi_Connect = (Button) findViewById(R.id.oxi_connect_button);
-        oxi_Connect.setOnClickListener(new View.OnClickListener() {
+        //Configure button
+        mButtonConfig.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (!BTNoninScanner.isScanning()) {
-                    stopEverything();
-                    Utility.toastie(getApplicationContext(), "Scanning");
-                    BTNoninScanner.start();
-                } else {
-                    BTNoninScanner.stop();
+                if (mServerIP.getText().toString().isEmpty())
+                    Utility.toastie(getApplicationContext(), "Enter Server IP address!");
+                else {
+                    //Configure Network Manager and connect to server
+                    mNetworkFragment.configure("http://" + mServerIP.getText() + ":5000/api/", repo_name);
+
+                    if (!download) {
+                        mNetworkFragment.getUpdate();
+                    }
                 }
             }
         });
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mNetworkFragment.onDetach();
+        mNetworkFragment = null;
+        mMsgProcessor = null;
+        mFragManager = null;
+    }
+
+
+    //--------------------------------------------------------------------------------------------//
+    //  FUNCTIONS:
+    //--------------------------------------------------------------------------------------------//
 
     //When RadioGroup "Configuration" checked change
     private void doOnConfigChanged(RadioGroup group, int checkedId) {
@@ -120,21 +144,136 @@ public class ConfigActivity extends AppCompatActivity {
 
         switch (checkedRadioID) {
             case R.id.radioButton_SDFT:
+                repo_name = "SDFT"; //set name of server repository
                 break;
 
             case R.id.radioButton_HDFT:
+                repo_name = "HDFT"; //set name of server repository
                 break;
 
             case R.id.radioButton_HFFT:
+                repo_name = "HFFT"; //set name of server repository
                 break;
 
             case R.id.radioButton_custom_FFT:
+                repo_name = "custom_FF"; //set name of server repository
                 break;
 
             default: //equals to radioButton_SDFT
+                repo_name = "SDFT"; //set name of server repository
                 break;
         }
     }
+
+    /**
+     * save information to persistence storage
+     *
+     * @param repository
+     */
+    private void saveConfig(Repository repository) {
+        SharedPreferences.Editor editor = mSharedPref.edit();
+        editor.putString(Constants.JSON.INDEX, repository.getIndex());
+        editor.putString(Constants.JSON.VERSION, repository.getVersion());
+        editor.putString(Constants.JSON.DATE, repository.getDate());
+        editor.putString(Constants.JSON.CHECKSUM, repository.getChecksum());
+        editor.commit();
+    }
+
+    /**
+     * load information from persistence storage
+     *
+     * @return
+     */
+    private HashMap<String, String> getConfig() {
+        HashMap<String, String> dict = new HashMap<String, String>();
+        dict.put(Constants.JSON.INDEX, mSharedPref.getString(Constants.JSON.INDEX, null));
+        dict.put(Constants.JSON.VERSION, mSharedPref.getString(Constants.JSON.VERSION, null));
+        dict.put(Constants.JSON.DATE, mSharedPref.getString(Constants.JSON.DATE, null));
+        dict.put(Constants.JSON.CHECKSUM, mSharedPref.getString(Constants.JSON.CHECKSUM, null));
+
+        return dict;
+    }
+
+    /**
+     * print message to user interface
+     *
+     * @param text
+     */
+    private void printDebug(String text) {
+        mConsole.append(text);
+    }
+
+    //--------------------------------------------------------------------------------------------//
+    //  NETWORK MANAGER CALLBACKS:
+    //--------------------------------------------------------------------------------------------//
+
+    @Override
+    public void onDataAvailable(JSONObject json) {
+        mMsgProcessor.process(json);
+    }
+
+
+    @Override
+    public void printToTextBox(String text) {
+        printDebug(text);
+    }
+
+
+    @Override
+    public void onDownloadComplete(Repository repo) {
+        download = false;
+        Utility.toastie(getApplicationContext(), "Download complete");
+        mConsole.append("\r\nDownload Complete\r\n");
+        mMsgProcessor.verifyBitstream(repo);
+    }
+
+    //--------------------------------------------------------------------------------------------//
+    //  MESSAGE PROCESSOR CALLBACKS:
+    //--------------------------------------------------------------------------------------------//
+
+    @Override
+    public void onUpdateAvailable(Repository repo) {
+        //init download
+        download = true;
+        mConsole.append("\r\n" + repo.toString());
+
+        saveConfig(repo);   // disable for debugging
+        mNetworkFragment.download(repo, MainActivity.path);
+    }
+
+    @Override
+    public byte[] calculateHash(String file) {
+        return mFabricManager.calculateHashFromFile(MainActivity.path + "/" + file, MainActivity.path + "/" + blake2bDriver, MainActivity.path + "/" + blake2bHash);
+    }
+
+
+    @Override
+    public void onVerifiedBitstream(Repository repo, boolean valid) {
+        if (valid) {
+            mConsole.append("Bitstream verified\r\n");
+            mConsole.append("Reconfigure fabric\r\n");
+            mFabricManager.reconfigureFabric(MainActivity.path + "/" + repo.getFile(), MainActivity.path + "/" + reconfigDriver);
+        } else {
+            mConsole.append("Bitstream invalid\r\n");
+
+        }
+    }
+
+
+    @Override
+    public HashMap<String, String> getCurrentConfig() {
+        return getConfig();
+    }
+
+    //--------------------------------------------------------------------------------------------//
+    //  FABRIC MANAGER CALLBACKS:
+    //--------------------------------------------------------------------------------------------//
+
+    @Override
+    public void onFilterApplied() {
+        //change
+    }
+
 
 
 }
