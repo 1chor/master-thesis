82a263fbf71ac0f6538eb30d170bc72b141bde16
1chor <e1325347@student.tuwien.ac.at>
2020-12-19 20:38:53 +0100

    create testbench

---
+++ spectral-analysis/spectral-analysis.srcs/sim_1/new/xdft_tb.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 12/19/2020 08:25:35 PM
+-- Design Name: 
+-- Module Name: xdft_tb - bench
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity xdft_tb is
+--  Port ( );
+end xdft_tb;
+
+architecture bench of xdft_tb is
+
+begin
+
+
+end bench;
