e272f1e7297e6a5423674552635d1a394261133c
1chor <e1325347@student.tuwien.ac.at>
2021-01-06 12:07:55 +0100

    add option for normalised input
    already calculated or self calculation

--- spectral-analysis/matlab/run_xdft_test.m
+++ spectral-analysis/matlab/run_xdft_test.m
@@ -21,11 +21,20 @@ precision = 16;
 fwdinv = 0; % use forward transformation
 
 use_hex_float = true; % hex = true, float = false
+normalised_data = true; % true = use already normalised input data, flase = calculate it
 
 if (use_hex_float == true)
-    infile = './../TestData/input_TestData.txt';
+    if (normalised_data == true)
+        infile = './../TestData/input_TestData_norm.txt';
+    else
+        infile = './../TestData/input_TestData.txt';
+    end
 else
-    infile = './../TestData/input_TestData_float.txt';
+    if (normalised_data == true)
+        infile = './../TestData/input_TestData_norm_float.txt';
+    else
+        infile = './../TestData/input_TestData_float.txt';
+    end
 end
 
 % -----------------------------------------------------
@@ -48,20 +57,31 @@ if (use_hex_float == true)
     real_in = double(real_in); % convert the single float to double, needed by the dft
 end
 
-% Normalise values of the array to be between -1 and 1
-% original sign of the array values are maintained
-if abs(min(real_in)) > max(real_in)
-    max_range_value = abs(min(real_in));
-    min_range_value = min(real_in);
+if (normalised_data == true)
+    % limit data into range
+    for i=1:size
+        if (real_in(i) == 1)
+            real_in(i) = real_in(i)/(real_in(i)+2^(1-precision));
+        end
+    end
+    input = complex(transpose(real_in)); % convert to complex
+    norm_abs = 4.4371;% only valid with certain test data
 else
-    max_range_value = max(real_in);
-    min_range_value = -max(real_in);
+    % Normalise values of the array to be between -1 and 1
+    % original sign of the array values are maintained
+    if abs(min(real_in)) > max(real_in)
+        max_range_value = abs(min(real_in));
+        min_range_value = min(real_in);
+    else
+        max_range_value = max(real_in);
+        min_range_value = -max(real_in);
+    end
+
+    norm_abs = (max_range_value - min_range_value + 2^(1-precision));
+
+    norm_real_in = 2 .* real_in ./ norm_abs; % normalise values
+    input = complex(transpose(norm_real_in)); % convert to complex
 end
-
-norm_abs = (max_range_value - min_range_value + 2^(1-precision));
-norm_real_in = 2 .* real_in ./ norm_abs;
-
-input = complex(transpose(norm_real_in)); % convert to complex
 
 % -----------------------------------------------------
 % Call the bit accurate model
