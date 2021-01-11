ff8a1c88a445cddd656bdcd1d32b7837cdb3e1ac
1chor <e1325347@student.tuwien.ac.at>
2021-01-11 23:27:15 +0100

    created wrapper for Xilinx FFT

---
+++ ft_cores/xfft/xfft.srcs/sources_1/new/xfft_wrapper.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 01/11/2021 11:09:04 PM
+-- Design Name: 
+-- Module Name: xfft_wrapper - arch
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity xfft_wrapper is
+--  Port ( );
+end xfft_wrapper;
+
+architecture arch of xfft_wrapper is
+
+begin
+
+
+end arch;
